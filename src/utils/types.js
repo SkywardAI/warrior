c3981ab0d77fdf0b3f3d8447331df825e6d76d1e
cbh778899 <cbh778899@outlook.com>
2025-02-10 10:07:01 +1100

    update model id
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- src/utils/types.js
+++ src/utils/types.js
@@ -1,5 +1,5 @@
 export const MODELS = [
-    { label: 'Claude 3.5 Sonnet', value: 'anthropic.claude-3-5-sonnet-20240620-v1:0' },
+    { label: 'Claude 3.5 Sonnet V2', value: 'anthropic.claude-3-5-sonnet-20241022-v2:0' },
     { label: 'Claude 3 Haiku', value: 'anthropic.claude-3-haiku-20240307-v1:0' },
     { label: 'Claude 3 Sonnet', value: 'anthropic.claude-3-sonnet-20240229-v1:0' },
     { label: 'Titan Text G1 - Express', value: 'amazon.titan-text-express-v1' },
