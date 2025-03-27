bd8c8b3178b0394b785732e0463cc86ec58ca09a
cbh778899 <cbh778899@outlook.com>
2025-03-27 11:45:49 +1100

    Disable send message if there's no session added
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- frontend/src/components/fields/TextArea.jsx
+++ frontend/src/components/fields/TextArea.jsx
@@ -1,28 +1,33 @@
 import PropTypes from 'prop-types';
 import c from 'classnames';
-import { useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { useEffect } from 'react';
 
 function TextArea({ 
-    value, onUpdate, disabled, className, 
+    value, onUpdate, disabled, className, focusToggle,
     autoNewLine = true, maxRows = 5, rows = 1,
     ...attrs
 }) {
 
     const [autoRows, setRows] = useState(rows);
+    const ref = useRef();
 
-    function onInput(e) {
+    const onInput = useCallback((e) => {
         if (disabled) return;
         onUpdate(e.target.value);
-    }
+    }, [disabled, onUpdate]);
 
     useEffect(()=>{
         autoNewLine && setRows(Math.min(maxRows, value.split('\n').length));
     }, [value, autoNewLine, maxRows])
 
+    useEffect(()=>{
+        ref.current && !disabled && ref.current.focus();
+    }, [focusToggle, disabled])
+
     return (
         <textarea 
-            value={value} onInput={onInput}
+            value={value} onInput={onInput} ref={ref}
             className={c('custom-field', 'textarea', className, {disabled})}
             rows={autoRows}
             {...attrs}
@@ -35,6 +40,7 @@ TextArea.propTypes = {
     onUpdate: PropTypes.func.isRequired,
     disabled: PropTypes.bool,
     className: PropTypes.string,
+    focusToggle: PropTypes.bool,
     autoNewLine: PropTypes.bool,
     rows: PropTypes.number,
     maxRows: PropTypes.number,
